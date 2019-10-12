3f5f9366e8c0d8d04801d90a2c71fd51db46de03
Sharan Mohan <36114089+mohan-sharan@users.noreply.github.com>
2019-10-13 02:28:56 +0530

    Add files via upload

---
+++ Python plot + TMP006/TMP006.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include "Adafruit_TMP006.h"
+#define USE_USCI_B1 
+
+Adafruit_TMP006 tmp006;
+void printFloat(float value, int places) ;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Initalizes the TMP006 for operation and for I2C communication
+  if (! tmp006.begin(TMP006_CFG_8SAMPLE)) {
+    Serial.println("No sensor found");
+    while (1);
+  }
+}
+
+void loop()
+{
+  float objt = tmp006.readObjTempC();
+  float diet = tmp006.readDieTempC();
+  
+  Serial.print(objt); //Object Temperature
+  Serial.print(" -- ");
+  Serial.println(diet); //Die Temperature  
+
+  delay(1000);
+}
